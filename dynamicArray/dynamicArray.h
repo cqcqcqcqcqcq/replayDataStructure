2c6ce33ae2abc3038d650281d097336262f94322
cqcqcqcqcqcq <2644551343@qq.com>
2023-12-18 23:30:31 +0800

    [修改] 新增动态数组的节点

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -1,7 +1,13 @@
 #ifndef __DYNAMIC_ARRAY_H_
 #define __DYNAMIC_ARRAY_H_
 
+typedef int ELEMENTTYPE;
 /*避免头文件重复包含*/
-
+typedef struct dynamicArray
+{
+    ELEMENTTYPE *data;       /*数组的空间*/
+    int len;        /*数组的大小*/
+    int capacity;   /*数组的容量*/
+}dynamicArray;
 
 #endif // __DYNAMIC_ARRAY_H_
