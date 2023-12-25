6cba3b6cd990327a4acddb0ee19212af00da05d2
cqcqcqcqcqcq <2644551343@qq.com>
2023-12-25 15:52:16 +0800

    [新增]完善二叉搜索树的逻辑结构

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -1,18 +1,18 @@
-#include"binarySearchTree.h"
-#include<stdlib.h>
+#include "binarySearchTree.h"
+#include <stdlib.h>
+#include <string.h>
 
 /* 状态码 */
 enum STATUS_CODE
 {
-    NOT_FIND = -1,
     ON_SUCCESS,
     NULL_PTR,
     MALLOC_ERROR,
     INVALID_ACCESS,
 };
 
-/*二叉搜索树的初始化*/
-int BinarySearchTreeInit(BinarySearchTree **pBstree)
+/* 二叉搜索树的初始化 */
+int binarySearchTreeInit(BinarySearchTree **pBstree)
 {
     int ret = 0;
     BinarySearchTree * bstree = (BinarySearchTree *)malloc(sizeof(BinarySearchTree) * 1);
@@ -20,39 +20,88 @@ int BinarySearchTreeInit(BinarySearchTree **pBstree)
     {
         return MALLOC_ERROR;
     }
-    /*清除脏数据*/
+    /* 清除脏数据 */
     memset(bstree, 0, sizeof(BinarySearchTree) * 1);
-    /*树的初始化*/
+    /* 初始化树 */
     {
         bstree->root = NULL;
         bstree->size = 0;
     }
+
     #if 1
-    /*分配根结点*/
+    /* 分配根结点 */
     bstree->root = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
-    if (bstree->root = NULL;)
+    if (bstree->root == NULL)
     {
         return MALLOC_ERROR;
     }
-    /*清除脏数据*/
-    memset(bstree->root, 0, sizeof(BinarySearchTree) * 1);
-    /*初始化根节点*/
+    /* 清除脏数据 */
+    memset(bstree->root, 0, sizeof(BSTreeNode) * 1);
+    /* 初始化根结点 */
     {
         bstree->root->data = 0;
         bstree->root->left = NULL;
         bstree->root->right = NULL;
         bstree->root->parent = NULL;
     }
-    #endif
-
+    #endif 
+    
     *pBstree = bstree;
     return ret;
 }
 
-/*二叉搜索树的插入*/
-int BinarySearchTreeInsert(BinarySearchTree **pBstree, ELEMENTTYPE val)
+/* 二叉搜索树的插入 */
+int binarySearchTreeInsert(BinarySearchTree *pBstree, ELEMENTTYPE val)
 {
     int ret = 0;
+    
+    /* 空树 */
+    if (pBstree->size == 0)
+    {
+        /* 更新树的结点 */
+        (pBstree->size)++;
+
+        pBstree->root->data = val;
+        return ret;
+    }
+
+    /* travelNode 指向根结点 */
+    BSTreeNode * travelNode = pBstree->root;
+    BSTreeNode * parentNode = pBstree->root;
+
+    /* 确定符号: 到底放在左边还是右边 */
+    int cmp = 0;
+    while (travelNode != NULL)
+    {
+        /* 标记父结点 */
+        parentNode = travelNode;
+        cmp = val - travelNode->data;
+        /* 插入元素 < 遍历到的结点 */
+        if (cmp < 0)
+        {
+            travelNode = travelNode->left;
+        }
+        else if (cmp > 0)     /* 插入元素 > 遍历到的结点 */
+        {
+            travelNode = travelNode->right;
+        }
+        else
+        {
+            /* 插入元素 = 遍历到的结点 */
+            return ret;
+        }
+    }
+
+    if (cmp < 0)
+    {
+        parentNode->left = (val的结点);
+    }
+    else
+    {
+        parentNode->right = (val的结点);
+    }
+
+
 
     return ret;
 }
