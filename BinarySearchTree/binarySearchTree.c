9d257186fa47437795ad2f1bed62756bd6feb29e
cqcqcqcqcqcq <2644551343@qq.com>
2023-12-25 15:14:34 +0800

    [新增新增树的初始化和API接口的插入

---
+++ BinarySearchTree/binarySearchTree.c
@@ -0,0 +1,16 @@
+#include"binarySearchTree.h"
+
+
+/*二叉搜索树的初始化*/
+int BinarySearchTreeInit(BinarySearchTree **pBstree)
+{
+    int ret = 0;
+
+    return ret;
+}
+
+/*二叉搜索树的插入*/
+int BinarySearchTreeInsert(BinarySearchTree **pBstree, ELEMENTTYPE val)
+{
+    
+}
