fcbde755e2a2f33d3c7629e6086bfa3786f139ce
cqcqcqcqcqcq <2644551343@qq.com>
2023-12-27 10:49:25 +0800

    [修改]完善二叉搜索树的前驱结点

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -110,8 +110,7 @@ static int binarySearchTreeNodeIsLeaf(BSTreeNode *node)
 /* 获取当前结点的前驱结点 */
 static BSTreeNode * bstreeNodePreDecessor(BSTreeNode *node)
 {
-    /* 度为2 */
-    if (binarySearchTreeNodeHasTwochildrens(node))
+    if (node->left != NULL)
     {
         /* 度为2, 前驱结点是在左子树的右子树的右子树... */
         BSTreeNode *travelNode = node->left;
@@ -121,12 +120,14 @@ static BSTreeNode * bstreeNodePreDecessor(BSTreeNode *node)
         }
         return travelNode;
     }
-    /* 程序到这个地方一定是度为1 或者 度为0的. */
-
-
-    /* 度为1 */
-
-    /* 度为0 */
+    /* 程序执行到这个地方 说明一定没有左子树， 那就需要向父结点找 */
+    while (node->parent != NULL && node->parent->left)
+    {
+        node = node->parent;
+    }
+    /* node->parent == NULL. */
+    /*  node == node->parent->right. */
+    return node->parent;
 }
 
 /* 获取当前结点的后继结点 */
